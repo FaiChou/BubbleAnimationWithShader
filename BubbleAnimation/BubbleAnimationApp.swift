bccd8ec68552e489ac608fbb03d41de0db359824
FaiChou <faichou.zh@gmail.com>
2024-11-14 17:46:50 +0800

    Initial Commit

---
+++ BubbleAnimation/BubbleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleAnimationApp.swift
+//  BubbleAnimation
+//
+//  Created by 周辉 on 2024/11/14.
+//
+
+import SwiftUI
+
+@main
+struct BubbleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
